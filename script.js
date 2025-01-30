66f3ba9f959441976619edea4ba85aec2fb6f775
micahransom <165352579+micahransom@users.noreply.github.com>
2025-01-30 13:24:25 -0800

    creative

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+    particlesJS("particles-js", {
+        particles: {
+            number: { value: 100, density: { enable: true, value_area: 800 } },
+            color: { value: "#ffffff" },
+            shape: { type: "circle" },
+            opacity: { value: 0.5, random: true },
+            size: { value: 4, random: true },
+            move: {
+                enable: true,
+                speed: 2,
+                direction: "none",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                bounce: false
+            }
+        },
+        interactivity: {
+            detect_on: "canvas",
+            events: {
+                onhover: { enable: true, mode: "repulse" },
+                onclick: { enable: true, mode: "push" },
+                resize: true
+            },
+            modes: {
+                repulse: { distance: 100, duration: 0.4 },
+                push: { particles_nb: 4 }
+            }
+            events: {
+                onhover: { enable: true, mode: "grab" },
+            },
+            modes: {
+                grab: { distance: 200, line_linked: { opacity: 0.5 } }
+            }
+        }
+    });
+});
